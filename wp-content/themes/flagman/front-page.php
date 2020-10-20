7b8a6186d453e83e16c923ff0f0ed0b442891faf
Askar <askar_1996@list.ru>
2020-10-20 23:15:42 +0600

    Add block in wordpress , add my custom theme

---
+++ wp-content/themes/flagman/front-page.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+   <title>Flagman</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+   <?php wp_body_open(); ?>
+   <!-- Заголовочный блок Header block-->
+   <header>
+      <div class="container">
+         <div class="row">
+            <div class="col-xl-12">
+               <div class="d-flex align-items-end">
+                  <h1 class="header__heading mr-3">ПЭ LB-52U ГОСТ</h1>
+                  <div class="header__hit">
+                     Хит продаж
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
+
+
+
+   <!-- Заголовочный блок Header block-->
+
+
+
+   <!-- Основной блок Home block -->
+   <section class="wp__home">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-auto col-md-6 col-sm-6 order-xl-1 order-lg-1 order-1">
+               <div class="home__price">
+                  <div class="home__price-heading">
+                     Цена (без НДС)
+                  </div>
+
+                  <span>от 107 ₽/кг </span>
+               </div>
+            </div>
+            <div class="col-lg-3  col-md-6 col-sm-6  ml-xl-2 order-xl-2 order-lg-3 order-3">
+               <div class="home__diametr">
+                  <div class="home__diametr-heading">
+                     Диаметр
+                  </div>
+                  <ul>
+                     <li>
+                        <span>2,5 мм</span>
+                     </li>
+                     <li>
+                        <span>3 мм</span>
+                     </li>
+                     <li>
+                        <span>4 мм</span>
+                     </li>
+                     <li>
+                        <span>5 мм</span>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-lg-3  col-md-6 col-sm-6  ml-xl-3 order-xl-3 order-lg-2 order-2">
+               <div class="home__pack">
+                  <div class="home__pack-heading">
+                     Упаковка
+                  </div>
+                  <ul>
+                     <li>
+                        <span>1 кг</span>
+                     </li>
+                     <li>
+                        <span>1,5 кг</span>
+                     </li>
+                     <li>
+                        <span>2,5 кг</span>
+                     </li>
+                     <li>
+                        <span>5 кг</span>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-lg-auto col-md-6 col-sm-6  ml-xl-2 order-xl-4 order-lg-4 order-4">
+               <div class="home__electro">
+                  <div class="home__electro-heading">
+                     Тип электродов Э50А
+                  </div>
+                  <div class="home__electro-img">
+
+                  </div>
+                  <!-- <img src="img/electro-img.svg" alt=""> -->
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-5 col-12 order-lg-5 order-5">
+               <div class="home__callback">
+                  <a href="#popup" class="popup-link">оформить заявку</a>
+               </div>
+            </div>
+         </div>
+
+      </div>
+   </section>
+   <!-- Основной блок Home block -->
+
+   <!-- Модальное окно modal -->
+   <div id="popup" class="popup">
+      <div class="popup__body">
+         <div class="popup__content">
+            <div class="popup__content-heading">
+               <a href="#header" class="popup__close close-popup"><img src="img/close.svg" alt=""></a>
+               <div class="popup__title">заявка на ПЭ LB-52U ГОСТ</div>
+            </div>
+            <div class="popup__text">
+               <form action="#" method="post" id="new-form">
+                  <input name="user_name" type="text" placeholder="Имя" required="">
+                  <input name="user_phone" type="text" placeholder="Телефон" required="">
+                  <input name="user_offer" type="text" placeholder="Email" required="">
+                  <label class="callback__checkbox-second">
+                     <input type="checkbox" required="">
+                     <span class="checkmark"></span>
+                     <a href="#popup" class="popup__politic">Даю согласие на обработку персональных данных</a><br><br>
+                  </label>
+                  <input type="submit" name="submit" class="btn send-btn" value="Отправить">
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- Модальное окно modal -->
+
+
+   <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/flagman/functions.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * flagman functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package flagman
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+if ( ! function_exists( 'flagman_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function flagman_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on flagman, use a find and replace
+		 * to change 'flagman' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'flagman', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'flagman' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'flagman_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'flagman_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function flagman_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'flagman_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'flagman_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function flagman_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'flagman' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'flagman' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'flagman_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function flagman_scripts() {
+	wp_enqueue_style( 'flagman-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_style_add_data( 'flagman-style', 'rtl', 'replace' );
+	wp_enqueue_style('flagman-bg', get_template_directory_uri() . '/assets/css/bootstrap-grid.css');
+	wp_enqueue_style('flagman-custom', get_template_directory_uri() . '/assets/css/custom.css');
+	wp_enqueue_style('flagman-responsive', get_template_directory_uri() . '/assets/css/responsive.css');
+
+
+
+	wp_enqueue_script('flagman-jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', array(), _S_VERSION, true);
+	wp_enqueue_script('flagman-customjs', get_template_directory_uri() . '/assets/js/custom.js', array(), _S_VERSION, true);
+
+	wp_enqueue_script( 'flagman-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'flagman_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
